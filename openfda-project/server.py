4dc0aec7b0e106eceeed1254351b67196d938cab
andresroman1999 <advmunoz@gmail.com>
2018-05-15 20:48:32 +0200

    ASDFGk

--- openfda-project/server.py
+++ openfda-project/server.py
@@ -103,7 +103,7 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             if pars_limit[0] == "limit":
                 limit = int(pars_limit[1])
 
-
+#######################################################################################################################
 #A PARTIR DE AQUI SE TRATAN LAS POSIBLES TERMINACIONES DE LA URL, SEGÚN "PATH" CONTENGA O NO LAS RUTAS PEDIDAS PARA ESTA PRÁCTICA.
 #SE INCLUYEN TAMBIÉN ALGUNAS DE LAS EXTENSIONES OPCIONALES
         #"/"----dirige a la página principal de servidor gracias a la funcion.
@@ -170,14 +170,16 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             resultado_html = self.devuelve_web(peligros)
 
             self.wfile.write(bytes(resultado_html, "utf8"))
+########################################################################################################################
         #En los dos casos siguientes el código utilizado es muy parecido.
         elif 'searchCompany' in self.path:
 
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
-        #Se construye la URL (gracias a las variables creadas al iniciode la práctica)y se utilizan los distintos metodos de la API para buscar los datos en "results"
-        #de forma similar  a prácticas anteriores
+        #Se construye la URL (gracias a las variables creadas al inicio de la práctica)y se utilizan los distintos metodos
+        # de la API para buscar los datos en "results"
+        #de forma similar  a prácticas anteriores.Los datos requeridos se guardan en listas de las que luego se extraerán.
             limit = 10
             comp=self.path.split('=')[1]
             companies = []
@@ -186,15 +188,17 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             r1 = conn.getresponse()
             data1 = r1.read()
             data = data1.decode("utf8")
-        #La diferenciia radica en que esta vez el usuario de la pagina interviene en la "construcción" de la url,definiendo el nombre concreto de, en este caso,
+        #La diferenciia radica en que esta vez el usuario de la pagina interviene en la "construcción" de la url,definiendo
+        #el nombre concreto de, en este caso,
         #la coompañia.
             datosofda = json.loads(data)
             events_search_comp = datosofda['results']
-
+        #Una vez obtenidos los datos, se imprimen por pantalla
             for event in events_search_comp:
                 companies.append(event['openfda']['manufacturer_name'][0])
             resultado_html = self.devuelve_web(companies)
             self.wfile.write(bytes(resultado_html, "utf8"))
+
         ##COMO EL CASO ANTERIOR PERO CON EL MEDICAMENTO QUE EL USUARIO ELIJA
         elif 'searchDrug' in self.path:
 
@@ -221,8 +225,10 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
 
             resultado_html = self.devuelve_web(drugs)
             self.wfile.write(bytes(resultado_html, "utf8"))
-
+#######################################################################################################################
         #En los siguientes casos, se modifican headers y el tipo de error únicamente.
+        #Se utiliza el mismo criterio que en los anteriores pero esta vez deniega el acceso ya sea porque
+        #se ha definido como una ruta con acceso restringido, o pporque directamente no existe(404)
         elif 'secret' in self.path:
             self.send_error(401)
             self.send_header('WWW-Authenticate', 'Basic realm="Mi servidor"')
@@ -237,7 +243,7 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             self.send_error(404)
             self.send_header('Content-type', 'text/plain; charset=utf-8')
             self.end_headers()
-            
+
 
         return
 
