a7a989b9ff11aa38355b52d63b422b9f93c11d15
andresroman1999 <advmunoz@gmail.com>
2018-05-15 11:05:28 +0200

    ASDFGk

--- openfda-project/server.py
+++ openfda-project/server.py
@@ -71,6 +71,7 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
                         """
         return list_html
     def devuelve_resultados_genericos (self, limit=10):
+
         conn = http.client.HTTPSConnection(self.ofda_api_url)
         conn.request("GET", self.ofda_api_evento + "?limit="+str(limit))
         print (self.ofda_api_evento + "?limit="+str(limit))
@@ -98,9 +99,6 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             print("NO SE HAN OBTENIDO PARAMETROS")
 
 
-
-
-
         if self.path=='/':
 
             self.send_response(200)
@@ -118,9 +116,9 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
 
             medicamentos = []
             resultados = self.devuelve_resultados_genericos(limit)
-            for resultado in resultados:
-                if ('generic_name' in resultado['openfda']):
-                    medicamentos.append (resultado['openfda']['generic_name'][0])
+            for r in resultados:
+                if ('generic_name' in r['openfda']):
+                    medicamentos.append (r['openfda']['generic_name'][0])
                 else:
                     medicamentos.append('Desconocido')
             resultado_html = self.devuelve_web (medicamentos)
@@ -132,14 +130,14 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             self.send_header('Content-type', 'text/html')
             self.end_headers()
 
-            companies = []
+            comp = []
             resultados = self.devuelve_resultados_genericos (limit)
-            for resultado in resultados:
-                if ('manufacturer_name' in resultado['openfda']):
-                    companies.append (resultado['openfda']['manufacturer_name'][0])
+            for r in resultados:
+                if ('manufacturer_name' in r['openfda']):
+                    comp.append (r['openfda']['manufacturer_name'][0])
                 else:
-                    companies.append('Desconocido')
-            resultado_html = self.devuelve_web(companies)
+                    comp.append('Desconocido')
+            resultado_html = self.devuelve_web(comp)
             self.wfile.write(bytes(resultado_html, "utf8"))
 
         elif 'listWarnings' in self.path:
@@ -148,14 +146,37 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             self.send_header('Content-type', 'text/html')
             self.end_headers()
 
-            warnings = []
+            warnings_lista = []
             resultados = self.devuelve_resultados_genericos (limit)
-            for resultado in resultados:
-                if ('warnings' in resultado):
-                    warnings.append (resultado['warnings'][0])
+            for r in resultados:
+                if ('warnings' in r):
+                    warnings_lista.append (r['warnings'][0])
                 else:
-                    warnings.append('Desconocido')
-            resultado_html = self.devuelve_web(warnings)
+                    warnings_lista.append('Desconocido')
+            resultado_html = self.devuelve_web(warnings_lista)
+            self.wfile.write(bytes(resultado_html, "utf8"))
+
+        elif 'searchCompany' in self.path:
+
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+
+            limit = 10
+            comp=self.path.split('=')[1]
+            companies = []
+            conn = http.client.HTTPSConnection(self.ofda_api_url)
+            conn.request("GET", self.ofda_api_evento + "?limit=" + str(limit) + self.ofda_api_comp + comp)
+            r1 = conn.getresponse()
+            data1 = r1.read()
+            data = data1.decode("utf8")
+
+            datosofda = json.loads(data)
+            events_search_comp = datosofda['results']
+
+            for event in events_search_comp:
+                companies.append(event['openfda']['manufacturer_name'][0])
+            resultado_html = self.devuelve_web(companies)
             self.wfile.write(bytes(resultado_html, "utf8"))
 
         elif 'searchDrug' in self.path:
@@ -173,37 +194,17 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             r1 = conn.getresponse()
             data1 = r1.read()
             data = data1.decode("utf8")
-            biblioteca_data = json.loads(data)
-            events_search_drug = biblioteca_data['results']
-            for resultado in events_search_drug:
-                if ('generic_name' in resultado['openfda']):
-                    drugs.append(resultado['openfda']['generic_name'][0])
+            datosofda2 = json.loads(data)
+            events_search_drug = datosofda2['results']
+            for r in events_search_drug:
+                if ('generic_name' in r['openfda']):
+                    drugs.append(r['openfda']['generic_name'][0])
                 else:
                     drugs.append('Desconocido')
 
             resultado_html = self.devuelve_web(drugs)
             self.wfile.write(bytes(resultado_html, "utf8"))
-        elif 'searchCompany' in self.path:
 
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-
-            limit = 10
-            company=self.path.split('=')[1]
-            companies = []
-            conn = http.client.HTTPSConnection(self.ofda_api_url)
-            conn.request("GET", self.ofda_api_evento + "?limit=" + str(limit) + self.ofda_api_comp + company)
-            r1 = conn.getresponse()
-            data1 = r1.read()
-            data = data1.decode("utf8")
-            biblioteca_data = json.loads(data)
-            events_search_company = biblioteca_data['results']
-
-            for event in events_search_company:
-                companies.append(event['openfda']['manufacturer_name'][0])
-            resultado_html = self.devuelve_web(companies)
-            self.wfile.write(bytes(resultado_html, "utf8"))
 
         elif 'secret' in self.path:
             self.send_error(401)
@@ -214,7 +215,7 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             self.send_error(404)
             self.send_header('Content-type', 'text/plain; charset=utf-8')
             self.end_headers()
-            self.wfile.write("WHAT IS  '{}'?".format(self.path).encode())
+            #self.wfile.write("WHAT IS  '{}'?".format(self.path).encode())
 
         return
 
