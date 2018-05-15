01ff28676922fa9a00aa535d571118d1c20991c9
andresroman1999 <advmunoz@gmail.com>
2018-05-15 15:11:50 +0200

    ASDFGk

--- openfda-project/server.py
+++ openfda-project/server.py
@@ -5,14 +5,16 @@ import json
 import socketserver
 PORT=8000
 ########################################################################################################################
+#LA CLASE DEL SERVIDOR
 class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
 
+#Estas variables servirán para construir la url al completo, con las "terminaciones" adecuadas para caso
     ofda_api_url="api.fda.gov"
     ofda_api_evento="/drug/label.json"
     ofda_api_drug='&search=active_ingredient:'
     ofda_api_comp='&search=openfda.manufacturer_name:'
 
-
+#La siguiente función determina como se visualizará la página en el navegador
     def get_pag_principal(self):
         html = """
             <html>
@@ -51,6 +53,8 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             </html>
                 """
         return html
+#En el caso de que se seleccione una de las dos listas disponibles, esta función devolverá los resultados obtenidos
+#al buscarse los datos pedidos, extrayéndolos de la lista.
     def devuelve_web (self, lista):
         list_html = """
                             <html>
@@ -69,17 +73,20 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
                             </html>
                         """
         return list_html
+
+#"results" contiene los datos de los distintos medicamentos de OPENFDA. Esta función guarda dichos datos en funcion de
+#los que se le exijan.
     def devuelve_resultados_genericos (self, limit=10):
 
         conn = http.client.HTTPSConnection(self.ofda_api_url)
         conn.request("GET", self.ofda_api_evento + "?limit="+str(limit))
-        print (self.ofda_api_evento + "?limit="+str(limit))
+        #print (self.ofda_api_evento + "?limit="+str(limit))
         r1 = conn.getresponse()
         data_raw = r1.read().decode("utf8")
         data = json.loads(data_raw)
         resultados = data['results']
         return resultados
-
+#Función para que el servidor soporte la petición tipo GET
     def do_GET(self):
         recursos = self.path.split("?")
         if len(recursos) > 1:
@@ -93,11 +100,11 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             pars_limit = parametros.split("=")
             if pars_limit[0] == "limit":
                 limit = int(pars_limit[1])
-                print("Limit: {}".format(limit))
+                #print("Limit: {}".format(limit))
         else:
             print("NO SE HAN OBTENIDO PARAMETROS")
-
-
+#A PARTIR DE AQUI SE TRATAN LAS POSIBLES TERMINACIONES DE LA URL, SEGÚN "PATH" CONTENGA O NO LAS RUTAS.
+        #"/"----dirige a la página principal de servidor gracias a la funcion.
         if self.path=='/':
 
             self.send_response(200)
@@ -107,7 +114,8 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             html=self.get_pag_principal()
 
             self.wfile.write(bytes(html, "utf8"))
-
+        #devuelve una lista de medicamentos con el ingrediente activo solicitado.La funcion deuelve_resultados_genericos
+        #extrae los datos y los guarda en una lista.De ella se extraerán y se mostrarán por pantalla.(self.devuelve_web)
         elif 'listDrugs' in self.path:
 
             self.send_response(200)
@@ -124,7 +132,7 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             resultado_html = self.devuelve_web (medicinas)
 
             self.wfile.write(bytes(resultado_html, "utf8"))
-
+        #IGUAL QUE EL CASO ANTERIOR PERO CON COMPAÑIAS
         elif 'listCompanies' in self.path:
 
             self.send_response(200)
@@ -137,25 +145,25 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
                 if ('manufacturer_name' in r['openfda']):
                     comp.append (r['openfda']['manufacturer_name'][0])
                 else:
-                    comp.append('Desconocido')
+                    comp.append('UNKNOWN')
             resultado_html = self.devuelve_web(comp)
 
             self.wfile.write(bytes(resultado_html, "utf8"))
-
+        #IGUAL QUE EN EL CASO ANTERIOR PERO CON PELIGROS/EFECTOS SECUNDARIOS DE LOS MEDICAMENTOS
         elif 'listWarnings' in self.path:
 
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
 
-            warnings_lista = []
+            peligros = []
             resultados = self.devuelve_resultados_genericos (limit)
             for r in resultados:
                 if ('warnings' in r):
-                    warnings_lista.append (r['warnings'][0])
+                    peligros.append (r['warnings'][0])
                 else:
-                    warnings_lista.append('Desconocido')
-            resultado_html = self.devuelve_web(warnings_lista)
+                    peligros.append('Desconocido')
+            resultado_html = self.devuelve_web(peligros)
 
             self.wfile.write(bytes(resultado_html, "utf8"))
 
@@ -167,9 +175,9 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
 
             limit = 10
             comp=self.path.split('=')[1]
-            companies = []
+            comps = []
             conn = http.client.HTTPSConnection(self.ofda_api_url)
-            conn.request("GET", self.ofda_api_evento + "?limit=" + str(limit) + self.ofda_api_comp + comp)
+            conn.request("GET", self.ofda_api_evento + "?limit=" + str(limit) + self.ofda_api_comp + comps)
             r1 = conn.getresponse()
             data1 = r1.read()
             data = data1.decode("utf8")
@@ -178,8 +186,8 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             events_search_comp = datosofda['results']
 
             for event in events_search_comp:
-                companies.append(event['openfda']['manufacturer_name'][0])
-            resultado_html = self.devuelve_web(companies)
+                comps.append(event['openfda']['manufacturer_name'][0])
+            resultado_html = self.devuelve_web(comps)
             self.wfile.write(bytes(resultado_html, "utf8"))
 
         elif 'searchDrug' in self.path:
@@ -207,12 +215,18 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
 
             resultado_html = self.devuelve_web(drugs)
             self.wfile.write(bytes(resultado_html, "utf8"))
-
+            
+        #En los siguientes casos, se modifican headers y el tipo de error únicamente.
         elif 'secret' in self.path:
             self.send_error(401)
             self.send_header('WWW-Authenticate', 'Basic realm="Mi servidor"')
             self.end_headers()
         ###########################REDIRECT????????###############################
+        #Un problema con el script de correción impedia implementar la extensión "redirect" ya que
+        #no reeconocia 302 como  error.CODIGO DEL SCRIP CORRECTOR :
+        ######self.assertEqual(resp.status_code, 200 )#######
+
+
         else:
             self.send_error(404)
             self.send_header('Content-type', 'text/plain; charset=utf-8')
